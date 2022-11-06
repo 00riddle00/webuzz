d34953c40c4746fb7ecc99b93d7b70936d95f719
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2022-11-06 17:35:45 +0200

    Add Heroku in config options.

--- config.py
+++ config.py
@@ -147,6 +147,7 @@ config = {
     "development": DevelopmentConfig,
     "testing": TestingConfig,
     "production": ProductionConfig,
+    'heroku': HerokuConfig,
     "docker": DockerConfig,
     "unix": UnixConfig,
     "default": DevelopmentConfig,
